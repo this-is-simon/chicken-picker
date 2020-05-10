8f6a2c93dbb3265422c71bf912b316b82da7bae8
Simon Atkins <simonatk@gmail.com>
2020-05-10 22:13:48 +0100

    Install styled-components and add basic styling to Header navigation links

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,29 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
+import styled from 'styled-components';
+
+const StyledHeader = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: baseline;
+`
+
+const StyledLink = styled(Link)`
+    color: tomato;
+    padding: 0 20px;
+`
 
 class Header extends Component {
     render() {
         return (
-            <div className='header'>
+            <StyledHeader>
                 <h1>ChickenPicker</h1>
                 <nav>
-                    <Link to="/">Home</Link>
-                    <Link to="/about">About</Link>
-                    <Link to="/contact">Contact</Link>
+                    <StyledLink to="/">Home</StyledLink>
+                    <StyledLink to="/about">About</StyledLink>
+                    <StyledLink to="/contact">Contact</StyledLink>
                 </nav>
-            </div>
+            </StyledHeader>
         )
     }
 }
